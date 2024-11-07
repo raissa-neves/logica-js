26c9b0ac0b691b891fef4ccf0c637299d7fb5c4b
raissa-neves <raissa.nsanches@gmail.com>
2024-11-07 20:40:34 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+    alert('Welcome to the secret number game!');
+    let numeroSecreto = 5;
+    let chute = prompt('Choose a number between 1 and 10');
+
+    if (chute == numeroSecreto) {
+        console.log('That is it! You found out the secret number (5)');
+    };
